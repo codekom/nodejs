4be6452292470d8017594d1307f8fa3eee462a9c
Komal Sinha <kom809snh@gmail.com>
2019-05-07 00:58:12 +0530

    Add files via upload

--- templateEngine.js
+++ templateEngine.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+var urlencodedParser = bodyParser.urlencoded({extended:false});
 
 app.set('view engine','ejs');
+
+app.get('/',function(req,res){
+    res.render('index');
+});
+
+app.get('/contact',function(req,res){
+    res.render('contact',{qs:req.query});
+});
+
+app.post('/contact',urlencodedParser,function(req,res){
+    console.log(req.body);
+    res.render('contactRes',{data:req.body});
+});
 
 app.get('/person/:name',function(req,res){
     let userData = {age:23,hobbies:['eating','sleeping','guitar']};
